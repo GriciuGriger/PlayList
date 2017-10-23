6224f49c59877bef7187138394b241afba439dd0
GriciuGriger <d.grittner@o2.pl>
2017-10-23 15:13:44 +0200

    add files to the project

---
+++ src/com/danielgrittner/Album.java
@@ -0,0 +1,59 @@
+package com.danielgrittner;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            this.songs.add(new Song(title, duration));
+            return true;
+        }
+        System.out.println("Song's already on the playlist");
+        return false;
+    }
+
+    private Song findSong(String titleToBeFound) {
+        for (int i = 0; i < songs.size(); i++) {
+            if (songs.get(i).getTitle().equals(titleToBeFound)) {
+                return songs.get(i);
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if (index > 0 && (index <= this.songs.size())) {
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlayList(String title, LinkedList<Song> playList) {
+        Song checkedSong = findSong(title);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return false;
+    }
+
+
+}
+
+
+
